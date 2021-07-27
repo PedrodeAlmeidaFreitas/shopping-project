7b7d886db1214acaa185c1e298ca1067f1e0fb20
Pedro de Almeida <pedro.freitas@prudential.com>
2021-07-26 23:43:33 -0300

    Primitive navigation implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'shopping-project';
+  loadedFeature = 'recipes';
+
+  onNavigate(event: any) {
+    this.loadedFeature = event;
+  }
+
+  whatFeatureToShow(route: string) {
+    return this.loadedFeature === route;
+  }
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() onSelectRoute = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSelect(event: string) {
+    this.onSelectRoute.emit(event);
   }
-
 }
